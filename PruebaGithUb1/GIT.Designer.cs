70dff46e1060157894cf034c19180c4d0aeedb75
senaadsicta1@gmail.com <senaadsicta1@gmail.com>
2022-06-09 09:12:52 -0500

    ahhh

--- PruebaGithUb1/GIT.Designer.cs
+++ PruebaGithUb1/GIT.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GIT));
             this.label1 = new System.Windows.Forms.Label();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -37,22 +39,39 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 412);
+            this.label1.Location = new System.Drawing.Point(220, -2);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(192, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Version 3.0";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(245, 385);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(146, 53);
+            this.btnmostrar.TabIndex = 1;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 20);
+            this.textBox1.TabIndex = 2;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(429, 420);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 205);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 31);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "GUARDAAAAR";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ingrese un numero";
             // 
             // GIT
             // 
@@ -62,6 +81,8 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(646, 450);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnmostrar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "GIT";
@@ -74,6 +95,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
     }
 }
