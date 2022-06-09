998aa14d3d0aaa4caccde7a0f681b1615a0c5e09
senaadsicta1@gmail.com <senaadsicta1@gmail.com>
2022-06-09 07:19:32 -0500

    ahhhhhhhhhhhh

--- PruebaGithUb1/Form1.Designer.cs
+++ PruebaGithUb1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,6 +42,17 @@
             this.label1.Size = new System.Drawing.Size(192, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Version 1.0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(429, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GUARDAAAAR";
             // 
             // Form1
             // 
@@ -49,6 +61,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
@@ -61,6 +74,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PruebaGithUb1/Form1.cs
+++ PruebaGithUb1/Form1.cs
@@ -16,5 +16,10 @@ namespace PruebaGithUb1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            regrg
+        }
     }
 }
